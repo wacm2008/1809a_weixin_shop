850bcf8f7c41b5186cf1965b06df7acbf8a1dc36
wacm2008 <gl.star@163.com>
2019-04-23 13:35:48 +0000

    debug5

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -39,6 +39,16 @@
                         img+=v+',';
                         var node = "#imgs"+i;
                         $(node).attr('src',v);
+                        //上传图片
+                        wx.uploadImage({
+                            localId: v, // 需要上传的图片的本地ID，由chooseImage接口获得
+                            isShowProgressTips: 1, // 默认为1，显示进度提示
+                            success: function (r) {
+                                var serverId = r.serverId; // 返回图片的服务器端ID
+                                //alert('serverID: '+ serverId);
+                                console.log(r);
+                            }
+                        });
                     })
                     $.ajax({
                         url : '/jssdk/foto?img='+img,     //将上传的照片id发送给后端
