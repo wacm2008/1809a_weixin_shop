8cd80a863cd95976e9bd3c030f65b59d1d435e0f
wacm2008 <gl.star@163.com>
2019-04-30 01:48:15 +0000

    标签授权2

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -531,6 +531,10 @@ class WxController extends Controller
     }
     //标签
     public function etiqueta(){
+        $code=$_GET['code'];
+        $url='https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET').'&code=CODE&grant_type=authorization_code';
+        $response=json_decode(file_get_contents($url),ture);
+        print_r($response);
 
     }
 }
